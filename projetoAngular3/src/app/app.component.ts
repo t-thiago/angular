74857cc069c3b48a0a2e01b7672feec156c26aec
Thiago Teixeira(Notebook) <thiago3174@gmail.com>
2022-08-04 14:34:21 -0300

    220804

---
+++ projetoAngular3/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'AppThiago';
+  
+  count = 0;
+  constructor(){
+
+  };
+
+  ngOnInit(): void {
+      let interval =  setInterval(()=> {
+        this.count++;
+        if(this.count ===10){
+          clearInterval(interval)
+        }
+      }, 1000)
+  };
+}
